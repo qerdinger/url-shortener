1a9997fd57923bc8df7a1376332d96882042b89b
qerdinger <91551241+QuentinCraft@users.noreply.github.com>
2026-05-01 13:52:12 +0200

    refactor: changes default setup (name, ns, ...) to GoTo (gotosvc)

--- services/goto.svc/GoToFunction/src/main/java/goto/App.java
+++ services/goto.svc/GoToFunction/src/main/java/goto/App.java
@@ -1,4 +1,4 @@
-package helloworld;
+package gotosvc;
 
 import java.io.BufferedReader;
 import java.io.IOException;
--- services/goto.svc/GoToFunction/src/test/java/goto/AppTest.java
+++ services/goto.svc/GoToFunction/src/test/java/goto/AppTest.java
@@ -1,4 +1,4 @@
-package helloworld;
+package gotosvc;
 
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import static org.junit.Assert.assertEquals;
